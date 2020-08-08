d2ff0b74999845da29f0562d39e7246af67a59a6
Troydon <troyincarnate@gmail.com>
2020-08-08 18:43:31 +1000

    Create initial Django app

--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -19,8 +19,12 @@ Including another URLconf
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
+from django.conf.urls import include, url
+import DjangoApp1.views
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
+     url(r'^$', DjangoApp1.views.index, name='index'),
+    url(r'^home$', DjangoApp1.views.index, name='home'),
 ]
